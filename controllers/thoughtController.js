6b475da0e39504cf48cdc0e5998a4d3063a79a1a
Francisco <francisco.caldera2015@gmail.com>
2023-05-28 15:41:08 -0500

    fixed error in thought delete route

--- controllers/thoughtController.js
+++ controllers/thoughtController.js
@@ -72,7 +72,7 @@ module.exports = {
     // remove a thought
     async deleteThought(req, res) {
         try {
-            const thought = await Thought.findOneAndRemove({ _id: req.params.applicationId });
+            const thought = await Thought.findOneAndRemove({ _id: req.params.thoughtId });
 
             if (!thought) {
                 return res.status(404).json({ message: 'No thought with this id!' });
